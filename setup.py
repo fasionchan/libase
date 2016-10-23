74dbaed03bac7c4b99c521dbf3cb564e37009efc
Fasion Chan <fasionchan@gmail.com>
2016-10-23 10:55:24 +0800

    first empty publishable

---
+++ setup.py
@@ -0,0 +1,36 @@
+#!/usr/bin/env python
+# -*- encoding=utf8 -*-
+
+'''
+FileName:   setup.py
+Author:     Fasion Chan
+@contact:   fasionchan@gmail.com
+@version:   $Id$
+
+Description:
+
+Changelog:
+
+'''
+
+VERSION = '1.0'
+
+from setuptools import (
+    setup,
+    )
+
+setup(
+    name='libase',
+    version=VERSION,
+    author='Fasion Chan',
+    author_email='fasionchan@gmail.com',
+    packages=[
+        'libase',
+        ],
+    scripts=[
+        ],
+    package_data={
+        },
+    install_requires=[
+        ],
+    )
